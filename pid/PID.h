bc4a278c89ab3d7bd72cfd4c6b8e0d161c9bc47e
alvaro.sanchez <alvarosangar.x2@gmail.com>
2020-11-17 20:25:43 +0000

    Subscriber de altura e makefile do PID modificados

--- pid/PID.h
+++ pid/PID.h
@@ -1,7 +1,14 @@
 #ifndef PID_HEADER
 #define PID_HEADER
 
+#define domainImu 0
+#define domainHeight 1
+#define domainGps 2
+
+
+
 float pid_pitch(float entrada);
 float pid_roll(float entrada);
+float pid_height(float entrada);
 
 #endif /*PID_HEADER*/
